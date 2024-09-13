d4058cc7a1f8969f77352d41b46ac9255fc02bbe
Harold Martin <harold.martin@gmail.com>
2024-09-12 19:36:19 -0700

    add brands list

--- src/content/after.js
+++ src/content/after.js
@@ -1,4 +1,11 @@
-// this code will be executed after page load
+function getResourceContent(fileName) {
+  return fetch(chrome.runtime.getURL(fileName))
+  .then(resp => resp.json()); // important line
+}
+
 (function() {
   console.log('Script "after.js" executed after page load.');
+  getResourceContent("src/content/brands.json").then(content => {
+    console.log(content);
+  });
 })();
